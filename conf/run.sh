eeb9996194cfb6e55874c81d3eab58df0cee344e
Ying Wang <yingw787.personal@gmail.com>
2020-02-29 17:04:55 -0500

    Update run.sh add minimal configuration.

---
+++ conf/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Shell script for building and configuring the complete environment for
+# `docker_as_vagrant`.
+#
+# Assumptions:
+#   - `docker` is installed and available as $(which docker)
+#   - `git` is installed and available as $(which git)
+
+DOCKER=$(which docker)
+GIT=$(which git)
+
+GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
+DOCKER_IMAGE_NAME='docker_as_vagrant:latest'
+DOCKER_CONTAINER_NAME='docker_as_vagrant'
+
+$DOCKER build $GIT_REPO_ROOT/conf \
+    --tag $DOCKER_IMAGE_NAME
+
+CONTAINER_EXISTS=$($DOCKER ps -a --format '{{ .Names }}' --filter name=$DOCKER_CONTAINER_NAME)
+if [ -n "$CONTAINER_EXISTS" ];
+then
+    $DOCKER stop $DOCKER_CONTAINER_NAME && $DOCKER rm $DOCKER_CONTAINER_NAME
+fi
+
+$DOCKER run \
+    --name $DOCKER_CONTAINER_NAME \
+    --network=host \
+    --volume=$(pwd):/app \
+    --volume=$GIT_REPO_ROOT/src:/root \
+    -itd $DOCKER_IMAGE_NAME
